ee469593753d94e89a2a271c4145e736781721f2
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 <lpenrose@depaul.edu>
2022-12-30 19:32:45 -0600

    added preventDefault on submit

--- src/main/resources/static/javaScript/index.js
+++ src/main/resources/static/javaScript/index.js
@@ -4,12 +4,13 @@ let bookCoverLink = document.getElementById("book-cover-link")
 let bookSummary = document.getElementById("book-summary")
 let bookLocation = document.getElementById("book-location")
 let id = 0;
-
-
+let myForm = document.getElementById("my-form")
 
 
 function helloWorld(){
-
+    myForm.addEventListener("submit", function(event){
+        event.preventDefault()
+    });
     const data = {
         bookTitle:bookTitle.value,
         id:id,
