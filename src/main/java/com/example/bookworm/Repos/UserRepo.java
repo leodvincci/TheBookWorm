9ca39a658022a9e1b04facf52e1d108f401ed6ac
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 <lpenrose@depaul.edu>
2023-01-03 17:20:05 -0600

    Updated roleRepo

--- src/main/java/com/example/bookworm/Repos/UserRepo.java
+++ src/main/java/com/example/bookworm/Repos/UserRepo.java
@@ -4,4 +4,5 @@ import com.example.bookworm.Entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<UserEntity,Long> {
+    UserEntity findByUserName(String userName);
 }
